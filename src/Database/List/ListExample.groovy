fa88a3493b91c3b84678a2f5532ea69eebcf811e
Tim M <timothy.romero.manas@gmail.com>
2021-06-28 20:54:19 -0400

    Adding receipe for list

--- src/Database/List/ListExample.groovy
+++ src/Database/List/ListExample.groovy
@@ -32,7 +32,7 @@ class ListExample {
 
 
         // Add new value
-        print("Add new values in List")
+        print("Add new values in List -------")
         integerList.add(10)
         integerList.add(12)
         integerList.add(13)
@@ -44,7 +44,7 @@ class ListExample {
         printList()
 
         // Contains
-        println("Check if element appears in list")
+        println("Check if element appears in list -------")
         println("Is 10 present in intergerList ? " + integerList.contains(10))
         println("Is 'Tim' present in stringList ? " + stringList.contains('Tim'))
         println("Is array [100,200] present in nestedList ? " + nestedList.contains([100, 200]))
@@ -56,7 +56,7 @@ class ListExample {
         println()
 
         // get value
-        println("Get the values from a list")
+        println("Get the values from a list -------")
         println("Get the 5th element in integerList: " + integerList.get(4))
         println("Get the 3rd element from stringList: " + stringList.get(2))
         println("Get the 5 element on nestedList: " + nestedList.get(4))
@@ -64,25 +64,101 @@ class ListExample {
         println()
 
         // Check if Empty
-        println("Check if Empty")
+        println("Check if Empty -------")
         println("Is IntegerList Empty ? " + integerList.isEmpty())
         println("Is randomList Empty ? " + randomList.isEmpty())
         println()
 
         // Minus - Create new list without those specified
+        println("Minus - Create new list without those specified -------")
+        println("Before")
+        printList()
+
+        println("After")
+        println("Note: If element is a nested Array, you need to double the [[]]")
+        def minusIntegerList = integerList.minus([1,2,3,4,5])
+        def minusNestedList = nestedList.minus([[100, 200, 300]])
+        def minusMixedList = mixedList.minus([[10,20,30]])
+        println("minusIntegerList: " + minusIntegerList)
+        println("minusNestedList: " + minusNestedList)
+        println("minusMixedList: " + minusMixedList)
+        println()
 
         // Plus - Create new list With those specified
+        println("Plus - Create new list With those specified -------")
+        println("Before")
+        printList()
 
-        // Remove last item in the list
+        def plusIntegerList = integerList.plus([100,200,300])
+        def plusNestedList = nestedList.plus([[11, 22, 33]])
+        def plusMixedList = mixedList.plus([[10,20,30,40,50]])
+        println("plusIntegerList: " + plusIntegerList)
+        println("plusNestedList: " + plusNestedList)
+        println("plusMixedList: " + plusMixedList)
+        println()
+
+        // Remove First element in the list
+        println("Removing First item from list -------")
+        println("Before")
+        printList()
+
+        println("After")
+        println("Removing first item from IntegerList: " + integerList.pop())
+        println("Removing first item from NestedList: " + nestedList.pop())
+        println("Removing first item from MisedList: " + mixedList.pop())
+        printList()
+
+        // Remove Last element in the list
+        println("Removing Last item from list -------")
+        println("Before")
+        printList()
+
+        println("After")
+        println("Removing last item from IntegerList: " + integerList.removeLast())
+        println("Removing last item from NestedList: " + nestedList.removeLast())
+        println("Removing last item from MisedList: " + mixedList.removeLast())
+        printList()
+
 
         // Remove item at specified position
+        println("Removing item from specific indexes from list -------")
+        println("Before")
+        printList()
+
+        println("Remove 2nd item from integerList: " + integerList.remove(2))
+        println("Remove 3rd item from nestedList: " + nestedList.remove(3))
+        println("Remove 8th item from mixedList: " + mixedList.remove(8))
+        printList()
 
         // Fetch the size
+        println("Fetch size of list -------")
+        println("Size of integerList: " + integerList.size())
+        println("Size of nestedList: " + nestedList.size())
+        println("Size of mixedList: " + mixedList.size())
 
         // Reverse the list
+        println("Reverse List -------")
+        println("Before")
+        printList()
+
+        def reverseIntegerList = integerList.reverse()
+        def reverseNestedList = nestedList.reverse()
+        def reverseMixedList = mixedList.reverse()
+        println("reverseIntegerList : " + reverseIntegerList)
+        println("reverseNestedList : " + reverseNestedList)
+        println("reverseMixedList : " + reverseMixedList)
 
         // Sort the list
+        println("Sort the List -------")
+        println("Before")
+        printList()
 
+        def sortIntegerList = integerList.sort()
+        def sortNestedList = nestedList.sort()
+        def sortMixedList = mixedList.sort()
+        println("sortIntegerList : " + sortIntegerList)
+        println("sortNestedList : " + sortNestedList)
+        println("sortMixedList : " + sortMixedList)
 
     }
 
